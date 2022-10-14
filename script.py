67ea4828d3798ab02ea696cbe0c55820883e3f07
Andrew <74867190+Berendei-Jr@users.noreply.github.com>
2022-10-14 22:50:12 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -9,49 +9,52 @@ yandex_base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/downloa
 base = "http://retrolib.narod.ru/"
 #print("Окончание ссылки (html/book...): ")
 input = 'html/book_gg1.html'
+pages_num = 
 
 r = requests.get(base + input)
 
 html = BeautifulSoup(r.text, 'lxml')
 #print(html)
 table = html.find_all('table')[5]
+pages = (html.find_all('tr')[7]).find('b')
+page_num = int(pages.text[pages.text.find('[') + 1 : pages.text.find(']')])
 
 path = "Books"
 
 if (not os.path.exists(path)):
     os.mkdir(path)
 
-for row in table.find_all('tr'):
-    a = row.find_all('a', href = True)
-    if (len(a) > 0):
-        name = row.b.text
-        if (name != '[1]'):
-            url = a[0]['href']
-            if (url[0] == '.'):
-                end_url = url[3:]
-                url = base + end_url
+do {
+    for row in table.find_all('tr'):
+        a = row.find_all('a', href = True)
+        if (len(a) > 0):
+            name = row.b.text
+            if (name != '[1]'):
+                url = a[0]['href']
+                if (url[0] == '.'):
+                    end_url = url[3:]
+                    url = base + end_url
 
-            if (url.startswith('http://retrolib.narod.ru')):
-                f = open('Books/' + name + url[url.rfind('.'):], "wb")
-                #print("URL:", url)
-                ufr = requests.get(url)
-                f.write(ufr.content)
-                f.close()
-            else:
-                final_url = yandex_base_url + urlencode(dict(public_key=url))
-                response = requests.get(final_url)
-                download_url = response.json()['href']
-                
-                f = open('Books/tmp', "wb")
-                print("Download started/...")
-                download_response = requests.get(download_url)
-                f.write(download_response.content)
-                f.close()
-                mime = magic.Magic(mime=True)
-                mime = magic.Magic(mime=True)
-                res = mime.from_file('Books/tmp')
-                if (res == 'application/pdf'):
-                    os.rename('Books/tmp', 'Books/' + name + '.pdf')
+                if (url.startswith('http://retrolib.narod.ru')):
+                    f = open('Books/' + name + url[url.rfind('.'):], "wb")
+                    #print("URL:", url)
+                    ufr = requests.get(url)
+                    f.write(ufr.content)
+                    f.close()
                 else:
-                    os.rename('Books/tmp', 'Books/' + name + '.djvu')
-    
+                    final_url = yandex_base_url + urlencode(dict(public_key=url))
+                    response = requests.get(final_url)
+                    download_url = response.json()['href']
+                    
+                    f = open('Books/tmp', "wb")
+                    print("Download started/...")
+                    download_response = requests.get(download_url)
+                    f.write(download_response.content)
+                    f.close()
+                    mime = magic.Magic(mime=True)
+                    res = mime.from_file('Books/tmp')
+                    if (res == 'application/pdf'):
+                        os.rename('Books/tmp', 'Books/' + name + '.pdf')
+                    else:
+                        os.rename('Books/tmp', 'Books/' + name + '.djvu')
+} while ();
