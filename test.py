940136b3ab5501cc481a20b61f79541bda0b9651
Andrew <74867190+Berendei-Jr@users.noreply.github.com>
2022-10-14 22:50:54 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
+import requests
+import os
 import magic
-mime = magic.Magic(mime=True)
-print(mime.from_file("abc.pdf"))
+from bs4 import BeautifulSoup
+from urllib.parse import urlencode
+
+yandex_base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+ 
+base = "http://retrolib.narod.ru/"
+#print("Окончание ссылки (html/book...): ")
+input = 'html/book_gg23.html'
+
+r = requests.get(base + input)
+
+html = BeautifulSoup(r.text, 'lxml')
+#print(html)
+table = (html.find_all('tr')[7]).find('b')
+page_num = int(table.text[table.text.find('[') + 1 : table.text.find(']')])
+print(page_num)
